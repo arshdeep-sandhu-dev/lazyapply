394759d48b13d9db26f7bb8c6d10a0411b2ceae8
Arshdeep <Arsh.singh.sandhu1@gmail.com>
2026-01-12 19:03:45 -0600

    Made Api Utilize CSV

--- yob.py
+++ yob.py
@@ -1,27 +1,36 @@
-import csv
-from typing import Union, Optional
+#from typing import Union, Optional
 import uvicorn
-
-from fastapi import FastAPI, Query
-from pydantic import BaseModel
+from fastapi import FastAPI, Query, APIRouter
+from yob_scraper import read_yob_file, save_to_file
+import csv
+#from pydantic import BaseModel
 
 #app = FastAPI(docs_url=None, openapi_url=None)
 app = FastAPI()
+router = APIRouter(prefix="/api")
 
-@app.get("/")
+filePath = "./newgrad-jobs.csv"
+regexedFilePath = "./filtered-newgrad-jobs.csv"
+@router.get("/")
 def read_root():
     return {"Hello": "World"}
-@app.get("/getcsv")
+@router.get("/updatecsv")
+def update_csv():
+    try:
+        save_to_file(filePath)
+    except Exception as e:
+        return {"message": "Error updating CSV file: " + str(e)}
+    return {"message": "updated CSV file successfully"}
+@router.get("/getcsv")
 def get_csv(
     locationRegex: str = Query("", description="Location filter"),
     nameRegex: str = Query("", description="Name filter"),
 ):
-    print("Generating CSV file...")
-    print("Location Regex:", locationRegex)
-    print("Name Regex:", nameRegex)
+
     try :
-        yob_data = read_yob_file('./test/newgrad-jobs.csv', locationRegex,nameRegex)
-        with open('names.csv', 'w', newline='', encoding="utf-8-sig") as csvfile:
+        
+        yob_data = read_yob_file(filePath, locationRegex,nameRegex)
+        with open(regexedFilePath, 'w', newline='', encoding="utf-8-sig") as csvfile:
             fieldnames = ["Position Title","Date","Apply","Work Model","Location","Company","Company Size","Salary","Is New Grad"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
@@ -31,43 +40,10 @@ def get_csv(
     except Exception as e:
         return {"message": "Error creating CSV file: " + str(e)}
     
-    return {"message": "CSV file created successfully."}
+    return {"message": "Created filtered csv with " + str(len(yob_data)) + " rows", "yob_data": yob_data}
 
+app.include_router(router)
 
-
-def read_yob_file(file_path, locationRegex, nameRegex):
-    """
-    Reads a CSV file containing year of birth data and returns a list of dictionaries.
-    
-    Each dictionary
-    contains
-    Position Title,Date,Apply,Work Model,Location,Company,Company Size,Company Industry,Salary,Qualifications,H1b Sponsored,Is New Grad
-
-    """
-    yob_data = []
-    if locationRegex:
-        locationRegex = locationRegex.split(',')
-    if nameRegex:
-        nameRegex = nameRegex.split(',')
-    with open(file_path, mode='r', newline='', encoding='utf-8-sig') as csvfile:
-        reader = csv.DictReader(csvfile)
-        
-        for row in reader:
-            
-            
-                
-            exclude_keys = {"Company Industry", "Qualifications", "H1b Sponsored"}
-            d = {k: v for k, v in row.items() if k not in exclude_keys}
-            includeRowName = not nameRegex or any(name.strip().lower() in d["Position Title"].lower() for name in nameRegex)
-            includeRowLocation = not locationRegex or any(loc.strip().lower() in d["Location"].lower() for loc in locationRegex)
-            if (not includeRowLocation and d["Work Model"] != "Remote"):
-                continue
-            if (nameRegex and not includeRowName):
-                continue
-                
-            yob_data.append(d.copy())
-            d.clear()
-    return yob_data
 # Example usage:
 # yob_data = read_yob_file('yob_data.csv')
 # print(yob_data)
@@ -76,4 +52,5 @@ def read_yob_file(file_path, locationRegex, nameRegex):
 
 
 if __name__ == "__main__":
+    save_to_file(filePath)
     uvicorn.run(app, host="127.0.0.1", port=8000)
--- yob_scraper.py
+++ yob_scraper.py
@@ -1,8 +1,9 @@
 import requests
+import csv
 
 
 
-def save_to_file(data: str, filename: str) -> None:
+def save_to_file(filename: str) -> None:
     url = "https://airtable.com/v0.3/view/viwQUH792955wSCmV/downloadCsv?x-time-zone=America%2FChicago&x-user-locale=en&x-airtable-application-id=appjDG7vmPOm1pO7S&stringifiedObjectParams=%7B%22origin%22%3A%22viewMenuPopover%22%7D&requestId=req1kymnvN5gySm0H&accessPolicy=%7B%22allowedActions%22%3A%5B%7B%22modelClassName%22%3A%22view%22%2C%22modelIdSelector%22%3A%22viwQUH792955wSCmV%22%2C%22action%22%3A%22readSharedViewData%22%7D%2C%7B%22modelClassName%22%3A%22view%22%2C%22modelIdSelector%22%3A%22viwQUH792955wSCmV%22%2C%22action%22%3A%22getMetadataForPrinting%22%7D%2C%7B%22modelClassName%22%3A%22view%22%2C%22modelIdSelector%22%3A%22viwQUH792955wSCmV%22%2C%22action%22%3A%22readSignedAttachmentUrls%22%7D%2C%7B%22modelClassName%22%3A%22row%22%2C%22modelIdSelector%22%3A%22rows%20*%5BdisplayedInView%3DviwQUH792955wSCmV%5D%22%2C%22action%22%3A%22createDocumentPreviewSession%22%7D%2C%7B%22modelClassName%22%3A%22view%22%2C%22modelIdSelector%22%3A%22viwQUH792955wSCmV%22%2C%22action%22%3A%22downloadCsv%22%7D%2C%7B%22modelClassName%22%3A%22view%22%2C%22modelIdSelector%22%3A%22viwQUH792955wSCmV%22%2C%22action%22%3A%22downloadICal%22%7D%2C%7B%22modelClassName%22%3A%22row%22%2C%22modelIdSelector%22%3A%22rows%20*%5BdisplayedInView%3DviwQUH792955wSCmV%5D%22%2C%22action%22%3A%22downloadAttachment%22%7D%5D%2C%22shareId%22%3A%22shr763VHjlzPBDCgN%22%2C%22applicationId%22%3A%22appjDG7vmPOm1pO7S%22%2C%22generationNumber%22%3A0%2C%22expires%22%3A%222026-01-29T00%3A00%3A00.000Z%22%2C%22signature%22%3A%224a191072a447dfcfdf5d8da025222fcfae3f554533c55c09f24e2f80ed39043b%22%7D"
 
     headers = {
@@ -13,7 +14,42 @@ def save_to_file(data: str, filename: str) -> None:
     r = requests.get(url, headers=headers, timeout=60)
     r.raise_for_status()
 
-    with open("newgrad-jobs.csv", "wb") as f:
+    with open(filename, "wb") as f:
         f.write(r.content)
 
-    print("Saved newgrad-jobs.csv")
+
+def read_yob_file(file_path, locationRegex, nameRegex):
+    """
+    Reads a CSV file containing year of birth data and returns a list of dictionaries.
+    
+    Each dictionary
+    contains
+    Position Title,Date,Apply,Work Model,Location,Company,Company Size,Company Industry,Salary,Qualifications,H1b Sponsored,Is New Grad
+
+    """
+    yob_data = []
+    if locationRegex:
+        locationRegex = locationRegex.split(',')
+    if nameRegex:
+        nameRegex = nameRegex.split(',')
+    with open(file_path, mode='r', newline='', encoding='utf-8-sig') as csvfile:
+        reader = csv.DictReader(csvfile)
+        
+        for row in reader:
+            
+            
+                
+            exclude_keys = {"Company Industry", "Qualifications", "H1b Sponsored"}
+            d = {k: v for k, v in row.items() if k not in exclude_keys}
+            includeRowName = not nameRegex or any(name.strip().lower() in d["Position Title"].lower() for name in nameRegex)
+            includeRowLocation = not locationRegex or any(loc.strip().lower() in d["Location"].lower() for loc in locationRegex)
+            if (not includeRowLocation and d["Work Model"] != "Remote"):
+                continue
+            if (nameRegex and not includeRowName):
+                continue
+                
+            yob_data.append(d.copy())
+            d.clear()
+    return yob_data
+
+
